fe3a55557aa7f6601e095ce3b1835eaf5151bcac
Alethea Butler <alethea@alethea.io>
2014-02-09 23:54:00 -0500

    Refactored card_name to value conversion

--- hw01/cards.c
+++ hw01/cards.c
@@ -6,34 +6,29 @@
  *  prompt: string prompt to show
  *  card_name: buffer to store result in (max 2 characters)
  */
-void get_card_name(char *prompt, char *card_name);
+void get_card_name(char* prompt, char* card_name);
+
+/* Takes a card name and returns a value
+ * Returns 0 on unrecognized input, -1 on 'X' and 1-11 otherwise
+ *
+ * card_name: buffer containing user-inputed card name
+ */
+int name_to_value(char* card_name);
 
 int main()
 {
     char card_name[3];
     int count = 0;
-    while (card_name[0] != 'X') {
-        int val = 0;
+    int val;
+    while (1) {
         get_card_name("Enter the card_name: ", card_name);
-        switch(card_name[0]) {
-        case 'K':
-        case 'Q':
-        case 'J':
-            val = 10;
+        val = name_to_value(card_name);
+        if (val < 0) {
             break;
-        case 'A':
-            val = 11;
-            break;
-        case 'X':
+        } else if (val == 0) {
+            puts("I don't understand that value!");
             continue;
-        default:
-            val = atoi(card_name);
-            if ((val < 1) || (val > 10)) {
-                puts("I don't understand that value!");
-                continue;
-            }
-        }
-        if ((val > 2) && (val < 7)) {
+        } else if ((val > 2) && (val < 7)) {
             count++;
         } else if (val == 10) {
             count--;
@@ -43,7 +38,28 @@ int main()
     return 0;
 }
 
-void get_card_name(char *prompt, char *card_name) {
+void get_card_name(char* prompt, char* card_name) {
     puts(prompt);
     fgets(card_name, 3, stdin);
+}
+
+int name_to_value(char* card_name) {
+    int val;
+
+    switch(card_name[0]) {
+    case 'K':
+    case 'Q':
+    case 'J':
+        return 10;
+    case 'A':
+        return 11;
+    case 'X':
+        return -1;
+    default:
+        val = atoi(card_name);
+        if ((val > 1) && (val < 10)) {
+            return val;
+        }
+    }
+    return 0;
 }
