195ae727d95f7aa1779f145e4298f1fa12947c06
Alethea Butler <alethea@alethea.io>
2014-02-16 11:43:28 -0500

    Initial regex version of find_track, no error handling

--- hw02/find_track.c
+++ hw02/find_track.c
@@ -8,6 +8,8 @@ License: Creative Commons Attribution-ShareAlike 3.0
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
+#include <regex.h>
 
 #define NUM_TRACKS 5
 
@@ -38,7 +40,16 @@ void find_track(char search_for[])
 // Prints track number and title.
 void find_track_regex(char pattern[])
 {
-    // TODO: fill this in
+    int i;
+    regex_t regex;
+
+    regcomp(&regex, pattern, REG_NOSUB);
+    for (i = 0; i < NUM_TRACKS; i++) {
+        if (!regexec(&regex, tracks[i], 0, 0, 0)) {
+            printf("Track %i: '%s'\n", i, tracks[i]);
+        }
+    }
+    regfree(&regex);
 }
 
 // Truncates the string at the first newline, if there is one.
@@ -59,8 +70,8 @@ int main (int argc, char *argv[])
     fgets(search_for, 80, stdin);
     rstrip(search_for);
 
-    find_track(search_for);
-    //find_track_regex(search_for);
+    //find_track(search_for);
+    find_track_regex(search_for);
 
     return 0;
 }
