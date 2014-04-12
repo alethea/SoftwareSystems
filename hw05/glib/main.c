255ad141bc4008597238421ff6bfc66e77781140
Alethea Butler <alethea@alethea.io>
2014-04-12 19:11:17 -0400

    Working wordfreq, no error handling

--- hw05/glib/main.c
+++ hw05/glib/main.c
@@ -3,20 +3,31 @@
 #include "histogram.h"
 #include "parser.h"
 
+void histogram_print(gchar *term, gint32 count, gpointer user_data) {
+    g_printf("%s %d\n", term, count);
+}
+
 int main() {
     Parser *parser;
+    Histogram *histogram;
     GError *error;
     gchar *word;
 
     error = NULL;
     parser = parser_new(WORDFREQ_EBOOK, &error);
+    histogram = histogram_new();
+
     while (TRUE) {
         word = parser_read_word(parser, &error);
         if (word == NULL) {
             break;
         }
-        puts(word);
+        histogram_count(histogram, word);
     }
+    histogram_sort(histogram);
+    histogram_foreach(histogram, histogram_print, NULL);
+
     parser_free(parser);
+    histogram_free(histogram);
     return 0;
 }
